7f768b641b816284e83e59f5633cdba6aea6b77e
owengitdev <yanyida19790228@gmail.com>
2026-05-16 08:13:53 -0400

    Add files via upload

---
+++ My Project (C++) (OwenZ).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // age code
+    int age;
+    std::cin >> age;
+    std::cout << "Enter your age: ";
+    std::cout << age << std::endl;
+
+
+    // fruit code
+    int fruit;
+    std::cin >> fruit;
+
+    std::cout << "Enter your fruit: "<<fruit<<std::endl;
+
+}
+
