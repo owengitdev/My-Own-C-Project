333a349668bc6e3a8e993b13e107c17eb937b251
owengitdev <owenisaprosecondchannel@gmail.com>
2026-05-16 22:49:26 -0400

    Added some If/Else statements and Modify some code
    Added input prompts for age and fruit, and included conditional statements to check if the user is taken based on age.
    
    More updates will be out soon!

--- My Project (C++) (OwenZ).cpp
+++ My Project (C++) (OwenZ).cpp
@@ -4,16 +4,25 @@ using namespace std;
 int main() {
     // age code
     int age;
+    
+    cout << "Enter your age: ";
     std::cin >> age;
-    std::cout << "Enter your age: ";
     std::cout << age << std::endl;
 
 
     // fruit code
     int fruit;
+    cout << "Enter your fruit: ";
     std::cin >> fruit;
+    std::cout <<fruit<<std::endl;
 
-    std::cout << "Enter your fruit: "<<fruit<<std::endl;
+    // If and Else Statements in C++
+    if (age >= 10) {
+        std::cout << "You are already taken!" << std::endl;
+    } 
+    else {
+        std::cout << "You are not taken" << std::endl;
+    }
 
 }
 
